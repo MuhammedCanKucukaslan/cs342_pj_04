c7cb87fb5c4256abda3b12146d040dfc2cfd4a49
Giray Akyol <giray.akyol@ug.bilkent.edu.tr>
2022-05-01 22:56:03 +0300

    __u8 is not from stdlib, it is from types.h + changed pritnf("XXX\n") to pln("XXX")

--- fat.c
+++ fat.c
@@ -1,18 +1,22 @@
-#include <fcntl.h> // open()
-#include <stdio.h> // printf()
-#include <stdlib.h> // u_int8_t
-#include <string.h> // for strcmp() method
-#include <sys/mman.h> // stat()
-#include <unistd.h>   // and read()
-#include <linux/msdos_fs.h> // fat_boot_sector and msdos_dir_entry
 #include "fat.h"
+#include <fcntl.h>         // open()
+#include <linux/msdos_fs.h>// fat_boot_sector and msdos_dir_entry
+#include <stdio.h>         // printf()
+//#include <stdlib.h>        // u_int8_t
+#include <byteswap.h>
+#include <linux/types.h>
+#include <string.h>  // for strcmp() method
+#include <sys/mman.h>// stat()
+#include <unistd.h>  // and read()
+
+
 // declare the constants
 int data_start_sector;
 
 #define RESERVED_SECTOR_COUNT 32
 #define SECTOR_SIZE 512  // determined as such in last paragraph of page 1
-#define CLUSTER_SIZE 1024 // determined as such in the first line of page 2
-#define MEGA_TO 1048576 // 1024*1024
+#define CLUSTER_SIZE 1024// determined as such in the first line of page 2
+#define MEGA_TO 1048576  // 1024*1024
 
 
 // global variable declaration
@@ -23,31 +27,26 @@ unsigned int root_start_cluster;
 unsigned long int disk_size_in_bytes;
 
 
-
-
 int main(int argc, char **argv)
 {
     printf("%s : %d\n", argv[0], argc);
     pln("new line huh");
-    if (argc < 2)
-    {
+    if (argc < 2) {
         pln("Not enough argument is provided. fat -h");
     }
     // terrible condition
-    if (argc == 2)
-    {
+    if (argc == 2) {
         // todo print help
     }
 
     init(argv[1]);
 
-    if (strcmp(argv[2], "-v") == 0)
-    {
+    if (strcmp(argv[2], "-v") == 0) {
         pln("print info");
         print_v(argv[1]);
     }
 
-    printf("Tmp\n");
+    pln("Tmp");
     return 0;
 }
 
@@ -55,17 +54,17 @@ int main(int argc, char **argv)
 /*
  * initialize basic variables, especially the fat_boot_sector fbs
  */
-void init(char* disk_image_path){
+void init(char *disk_image_path)
+{
     int file = open(disk_image_path, O_RDONLY);
-    if (file == -1)
-    {
+    if (file == -1) {
         pln("Error opening file");
     }
     // read file
     u_int8_t sector[SECTOR_SIZE];
     read(file, sector, SECTOR_SIZE);
 
-    fbs = (struct fat_boot_sector *) sector; // type casting
+    fbs = (struct fat_boot_sector *) sector;// type casting
     // the following example shows how we can
     // access the related data from the sector
     sectors_per_cluster = fbs->sec_per_clus;
@@ -106,33 +105,34 @@ void init(char* disk_image_path){
 void print_v(char *disk_image)
 {
     int todo = -1;
-    char type[9];
-    for(int i = 0; i<8; i++) {
+    unsigned char type[9];
+    for (int i = 0; i < 8; i++) {
         type[i] = fbs->fat32.fs_type[i];
     }
     type[8] = '\0';
-    char label[12];
-    for(int i = 0; i<11; i++) {
+    unsigned char label[12];
+    for (int i = 0; i < 11; i++) {
         label[i] = fbs->fat32.vol_label[i];
     }
     label[11] = '\0';
 
-    printf("File system type: %s\n", type); // == NULL ? "FAT32" : "FAT16");
-    printf("Volume label: %s\n", label); //TODO
+    printf("File system type: %s\n", type);// == NULL ? "FAT32" : "FAT16");
+    printf("Volume label: %s\n", label);   //TODO
     printf("Number of sectors in disk: %d\n", num_sectors);
-    printf("Sector size in bytes: %lu\n", u8_to_ul(fbs->sector_size, 2) );
+    printf("Sector size in bytes: %lu\n", u8_to_ul(fbs->sector_size, 2));
     printf("Number of reserved sectors: %d\n", fbs->reserved);
     printf("Number of sectors per FAT table: %d\n", fbs->fat32.length);
     printf("Number of FAT tables: %u\n", fbs->fats);
     printf("Number of sectors per cluster: %d\n", fbs->sec_per_clus);
-    printf("Number of clusters = %d\n", (fbs->total_sect - (RESERVED_SECTOR_COUNT + num_fats * sectors_per_fat))/fbs->sec_per_clus); // TODO doesn't match the example
+    printf("Number of clusters = %d\n", (fbs->total_sect - (RESERVED_SECTOR_COUNT + num_fats * sectors_per_fat)) /
+                                                fbs->sec_per_clus);// TODO doesn't match the example
     printf("Data region starts at sector: %d\n", data_start_sector);
     printf("Root directory starts at sector: %d\n", data_start_sector);
     printf("Root directory starts at cluster: %d\n", root_start_cluster);
     printf("Disk size in bytes: %lu bytes\n", disk_size_in_bytes);
     printf("Disk size in Megabytes: %lu MB\n", (disk_size_in_bytes / MEGA_TO));
-    printf("Number of used clusters: %d\n", todo); // todo calculate
-    printf("Number of free clusters: %d\n", todo); // todo calculate
+    printf("Number of used clusters: %d\n", todo);// todo calculate
+    printf("Number of free clusters: %d\n", todo);// todo calculate
     // open file in raw and read
     /*
     int file = open(disk_image, O_RDONLY);
@@ -160,8 +160,7 @@ void word_to_binary(unsigned long int num, char *binary)
 {
     char ch = 'z';
     int length = 64;
-    for (int j = length - 1; j >= 0; j--)
-    {
+    for (int j = length - 1; j >= 0; j--) {
         // printf(" num: %d \n", num);
         ch = (num >> j & 1) == 1 ? '1' : '0';
 
@@ -209,15 +208,16 @@ int readcluster(int fd, unsigned char *buf, unsigned int cnum)
  * @param length
  * @return
  */
-unsigned long int u8_to_ul(__u8* arr, int length){
-     unsigned long int result = 0;
-     //int base = 256; // 2^8
-     int mult = 1;
-     for(int i = 0; i< length; i++) {
+unsigned long int u8_to_ul(__u8 *arr, int length)
+{
+    ulong result = 0;
+    //int base = 256; // 2^8
+    int mult = 1;
+    for (int i = 0; i < length; i++) {
         result += mult * arr[i];
         mult <<= 8;
-     }
-     return result;
+    }
+    return result;
 }
 
 void pln(char *input)
